ae6573e38a2b7a344ed0382351fa47dbd567a122
Muitsu <blankdahma@gmail.com>
2024-06-24 09:37:38 +0800

    add bg image

--- lib/screens/activity/activity_screen.dart
+++ lib/screens/activity/activity_screen.dart
@@ -1,3 +1,5 @@
+import 'package:eperumahan_bancian/components/bg_image.dart';
+import 'package:eperumahan_bancian/components/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
 class ActivityScreen extends StatefulWidget {
@@ -10,10 +12,45 @@ class ActivityScreen extends StatefulWidget {
 class _ActivityScreenState extends State<ActivityScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Center(child: Text("Activity"))],
+    return BgImage(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(child: Text("Activity")),
+            CustomTextField(
+              suffixIcon: Icons.unfold_more_rounded,
+              readOnly: true,
+              onTap: () {},
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("Carian")),
+            const SearchTextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchTextField extends StatelessWidget {
+  const SearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        hintText: 'Carian Kawasan',
+        prefixIcon: Icon(Icons.search),
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide(color: Colors.purple),
+        ),
       ),
     );
   }
