75bd35dfdc4786d9d5bdbcda03a0069571feb6f9
WanShing iOS <wanshing@vistakencana.com.my>
2024-06-24 12:57:58 +0800

    login screen ui update

--- lib/config/constants/app_images.dart
+++ lib/config/constants/app_images.dart
@@ -4,9 +4,12 @@ enum AppImages {
   icLock(path: "images/icon_lock.png"),
   icLogMasuk(path: "images/log_masuk.png"),
   bg(path: "assets/images/background.png"),
+  dbklLogo(path: "images/dbkl_logo.png"),
+  icId(path: "images/icon_id.png"),
+  icLock(path: "images/icon_lock.png"),
+  icLogMasuk(path: "images/log_masuk.png"),
   ;
 
   final String path;
-
   const AppImages({required this.path});
 }
