2222aa4de20a9d8a85bea0b3a38c6721b3766d80
WanShing iOS <wanshing@vistakencana.com.my>
2024-06-25 10:09:45 +0800

    solve conflict

--- lib/config/constants/app_images.dart
+++ lib/config/constants/app_images.dart
@@ -4,10 +4,6 @@ enum AppImages {
   icLock(path: "images/icon_lock.png"),
   icLogMasuk(path: "images/log_masuk.png"),
   bg(path: "assets/images/background.png"),
-  dbklLogo(path: "assets/images/dbkl_logo.png"),
-  icId(path: "assets/images/icon_id.png"),
-  icLock(path: "assets/images/icon_lock.png"),
-  icLogMasuk(path: "assets/images/log_masuk.png"),
   ;
 
   final String path;
