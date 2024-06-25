d04878485ec815da93f24c901923bcf824b6bf8e
WanShing iOS <wanshing@vistakencana.com.my>
2024-06-25 10:01:44 +0800

    login ui done

--- lib/config/constants/app_images.dart
+++ lib/config/constants/app_images.dart
@@ -4,10 +4,10 @@ enum AppImages {
   icLock(path: "images/icon_lock.png"),
   icLogMasuk(path: "images/log_masuk.png"),
   bg(path: "assets/images/background.png"),
-  dbklLogo(path: "images/dbkl_logo.png"),
-  icId(path: "images/icon_id.png"),
-  icLock(path: "images/icon_lock.png"),
-  icLogMasuk(path: "images/log_masuk.png"),
+  dbklLogo(path: "assets/images/dbkl_logo.png"),
+  icId(path: "assets/images/icon_id.png"),
+  icLock(path: "assets/images/icon_lock.png"),
+  icLogMasuk(path: "assets/images/log_masuk.png"),
   ;
 
   final String path;
