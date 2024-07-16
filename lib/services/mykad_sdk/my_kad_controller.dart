51e036a95d9413a50423a25bb62fa6a04e8cd5d9
Muitsu <blankdahma@gmail.com>
2024-07-16 16:43:46 +0800

    fix pubspec error

---
+++ lib/services/mykad_sdk/my_kad_controller.dart
@@ -0,0 +1,126 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'model/reader_response.dart';
+import 'my_kad_reader.dart';
+
+class MyKadController {
+  // Private constructor
+  MyKadController._privateConstructor();
+
+  // Singleton instance
+  static final MyKadController _instance =
+      MyKadController._privateConstructor();
+
+  // Factory constructor to return the singleton instance
+  factory MyKadController() {
+    return _instance;
+  }
+
+  StreamController<ReaderResponse>? controller;
+  MyKadModel? _myKadModel;
+  MyKidModel? _myKidModel;
+
+  // Get the stream
+  Stream<ReaderResponse>? get stream => controller?.stream;
+  MyKadModel? get getMyKad => _myKadModel;
+  MyKidModel? get getMyKid => _myKidModel;
+
+  // Initialize the StreamController & MyKadReader
+  void init({required bool verifyFP, required BuildContext context}) {
+    if (controller == null) {
+      controller = StreamController<ReaderResponse>.broadcast();
+      MyKadReader.callSDK();
+      MyKadReader.sdkListener(
+        context: context,
+        onIdle: () {
+          //Please insert card
+          setMessage(msg: "Please insert card");
+        },
+        onReadCard: () {
+          //Loading ...
+          setMyKadData(null);
+          setMessage(msg: "Loading read card...");
+        },
+        onSuccessCard: (data) async {
+          setMyKadData(data);
+          //Success read card
+          setMessage(msg: "Read card successful", data: data);
+          if (verifyFP) {
+            await MyKadReader.turnOnFP();
+            await addDelay(milisec: 3000);
+            await MyKadReader.getFPDeviceList();
+            await addDelay(milisec: 2000);
+            await connectAndScanFP();
+            setMessage(msg: "Please place your fingerprint at the scanner");
+          }
+        },
+        onErrorCard: () {
+          //Remove card and try again
+          setMessage(msg: "Remove card and try again");
+        },
+        onVerifyFP: () {
+          //Verifying Fingerprint
+          setMessage(msg: "Verifying Fingerprint...");
+        },
+        onSuccessFP: () {
+          //Success verify fingerprint
+          setMessage(msg: "User verification successful");
+        },
+        onErrorFP: () async {
+          //Please try again in 3 second
+          await addDelay(milisec: 3000);
+          await connectAndScanFP();
+          setMessage(msg: "Error: Please try again in 3 second");
+        },
+      );
+    }
+  }
+
+  Future addDelay({int milisec = 1500}) =>
+      Future.delayed(Duration(milliseconds: milisec));
+
+  void addData(ReaderResponse data) => controller?.sink.add(data);
+
+  Future connectAndScanFP() async {
+    await MyKadReader.disconnectFPScanner();
+    await addDelay();
+    await MyKadReader.connectFPScanner();
+    await addDelay();
+    await MyKadReader.readFingerprint();
+  }
+
+  void setMessage({SdkResponseModel? data, required String msg}) {
+    var resp =
+        ReaderResponse(data: data?.toJson().toString() ?? "", message: msg);
+    addData(resp);
+  }
+
+  void setMyKadData(SdkResponseModel? data) {
+    if (data == null) {
+      _myKadModel = null;
+      _myKidModel = null;
+      return;
+    }
+    final json = jsonDecode(data.data!);
+    if (data.isDataMykad()) {
+      _myKadModel = MyKadModel.fromJson(json);
+    } else {
+      _myKidModel = MyKidModel.fromJson(json);
+    }
+  }
+
+  // Close the StreamController
+  Future<void> close() async {
+    controller?.close();
+    controller = null;
+    setMyKadData(null);
+    await MyKadReader.disconnectFPScanner();
+    await addDelay();
+    await MyKadReader.turnOffFP();
+    await addDelay();
+    await MyKadReader.disposeListener();
+  }
+}
