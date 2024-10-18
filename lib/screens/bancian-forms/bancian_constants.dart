3ef92e973fb579b580b603025528a4c3ee8cdb29
Muitsu <blankdahma@gmail.com>
2024-10-18 16:26:26 +0800

    add missing item

--- lib/screens/bancian-forms/bancian_constants.dart
+++ lib/screens/bancian-forms/bancian_constants.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'anak_tanggungan/tanggungan_form.dart';
-import 'pasangan/pasangan_form.dart';
-import 'pendapatan/pendapatan_form.dart';
-import 'penghuni/penghuni_form_old.dart';
+// import 'anak_tanggungan/tanggungan_form.dart';
+// import 'pasangan/pasangan_form.dart';
+// import 'pendapatan/pendapatan_form.dart';
+// import 'penghuni/penghuni_form_old.dart';
 
-enum BancianForms {
-  penghuni(
-      title: "Maklumat Penghuni",
-      screen: PenghuniFormOld(
-        imgs: [],
-      )),
-  pasangan(title: "Maklumat Pasangan", screen: PasanganForm()),
-  pendapatan(title: "Maklumat Pendapatan", screen: PendapatanForm()),
-  anak(title: "Maklumat Anak & Tanggungan", screen: TanggunganForm()),
-  ;
+// enum BancianForms {
+//   penghuni(
+//       title: "Maklumat Penghuni",
+//       screen: PenghuniFormOld(
+//         imgs: [],
+//       )),
+//   pasangan(title: "Maklumat Pasangan", screen: PasanganForm()),
+//   pendapatan(title: "Maklumat Pendapatan", screen: PendapatanForm()),
+//   anak(title: "Maklumat Anak & Tanggungan", screen: TanggunganForm()),
+//   ;
 
-  final String title;
-  final Widget screen;
+//   final String title;
+//   final Widget screen;
 
-  const BancianForms({required this.title, required this.screen});
-}
+//   const BancianForms({required this.title, required this.screen});
+// }
 
 enum ReaderStatus {
   insert(
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:eperumahan_bancian/config/routes/routes_name.dart';
+import 'package:eperumahan_bancian/data/hive-manager/repository/login_pref.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,8 +8,12 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future.delayed(const Duration(milliseconds: 1500), () {
+      final isLoggedIn = LoginPreference().isTokenExpired() != null;
       Navigator.pushNamedAndRemoveUntil(
           context, RoutesName.login, (route) => false);
+      if (isLoggedIn) {
+        Navigator.pushNamed(context, RoutesName.home);
+      }
     });
 
     return const Scaffold(
